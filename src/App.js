6a25745e30290ab8508f31b412b297ea1fb023f5
edwincamilo <edcamilo2016@gmail.com>
2023-05-10 19:12:04 -0500

    promercommitcv

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+
+
+function curriculum() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className= "container--initial">
+          <div className="row mt-5">
+        <div className="col-5 md={{ size: 5, offset: 3 }}>"><h4 className='name--1'>Edwin Camilo Valencia Bustamante</h4></div>
+        <div className="col-7">
+
+          <h4 className='definition--2'>Basic Information</h4>
+          <p>Descripcion</p>
+          <h4 className='definition--2'>Education</h4>
+          <p>Descripcion </p>
+          <h4 className='definition--2'>Experience</h4>
+          <p>Descripcion</p>
+          <h4 className='definition--2'>Experience</h4>
+          <p>Descripcion</p>
+
+        </div>
+
+
+        
+        </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default curriculum;
