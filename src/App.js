6507eecffa71152161fb775b4d43567716419174
edwincamilo <edcamilo2016@gmail.com>
2023-05-13 14:09:20 -0500

    mas informacion personal

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function PersonalInfo(props) {
       <p>{props.email}</p>
       <p>{props.phone}</p>
       <p>{props.address}</p>
+
     </div>
   );
 }
@@ -18,10 +19,52 @@ function AboutMeDes(props) {
   return (
     <div className='information--aboutMe'>  
       <h4>{props.aboutMe}</h4>
+      
       <p>{props.text}</p>
     </div>
   );
 }
+
+function Titles(props){
+  return(
+    <div className='information--titles'>
+      <h4>{props.titles}</h4>
+      <ul>
+        <li><p>{props.degree1}</p></li>
+        <li><p>{props.school1}</p></li>  
+        <li><p>{props.degree2}</p></li>   
+        <li><p>{props.school2}</p></li>   
+        <li><p>{props.date}</p></li>   
+      </ul>
+    </div>
+  );
+}
+
+function Habilities(props){
+  return(
+    <div>
+      <h4>{props.Habilities}</h4>
+      <ul>
+        <p>{props.workGroup}</p>
+        <p>{props.responsability}</p>
+        <p>{props.collaboration}</p>
+      </ul>
+    </div>
+
+  );
+}
+
+function Languages(props){
+  return(
+    <div>
+      <h4>{props.languages}</h4>
+      <p>{props.Spanish}</p>
+      <p>{props.English}</p>
+    </div>
+
+  );
+}
+
 
 const myInfo = {
   name: 'Edwin Camilo Valencia Bustamante',
@@ -35,7 +78,28 @@ const myinfoAboutMe = {
   text:'Estudiante de Ingenieria en sistemas y telecomunicaciones de 5º semestre, con conicimientos en  Python, javascript, html, css, reactjs,Bases de datos, entre otros, con disponibilidad de seguir aprendiendo y trabajar'
   }
 
+const myTitles =
+  {
+  titles:'Titulos',
+  degree1: 'Bachiller academico',
+  degree2: 'Tecnico en instalaciones electricas residenciales',
+  school1: 'Instituto Tecnico Francisco Jose de Caldas',
+  school2: 'SENA',
+  date: '07-12-2019'
+}
 
+const myInfoHabilities={
+  Habilities:'Habilidades',
+  workGroup:'Trabajo en equipo',
+  responsability:'Responsabilidad',
+  collaboration:'Colaboracion'
+}
+
+const myInfoLanguages={
+  languages:'Idiomas',
+  Spanish:'Ingles : Avanzado',
+  English:'Español : Avanzado'
+}
 
 function Curriculum(props) {
   return(
@@ -50,7 +114,25 @@ function Curriculum(props) {
         aboutMe={props.aboutMe}
         text={props.text}
       />
-
+      <Titles
+        titles={props.titles}
+        degree1={props.degree1}
+        school1={props.school1}
+        degree2={props.degree2}
+        school2={props.school2}
+        date={props.date}
+      />
+      <Habilities
+        Habilities={props.Habilities}
+        workGroup={props.workGroup}
+        responsability={props.responsability}
+        collaboration={props.collaboration}
+      />
+      <Languages
+        languages={props.languages}
+        Spanish={props.Spanish}
+        English={props.English}
+      />
     </div>
   );
 }
@@ -65,6 +147,19 @@ function App() {
         address={myInfo.address}
         aboutMe={myinfoAboutMe.aboutMe}
         text={myinfoAboutMe.text}
+        titles={myTitles.titles}
+        degree1={myTitles.degree1}
+        school1={myTitles.school1}
+        degree2={myTitles.degree2}
+        school2={myTitles.school2}
+        date={myTitles.date}
+        Habilities={myInfoHabilities.Habilities}
+        workGroup={myInfoHabilities.workGroup}
+        responsability={myInfoHabilities.responsability}
+        collaboration={myInfoHabilities.collaboration}
+        languages={myInfoLanguages.languages}
+        Spanish={myInfoLanguages.Spanish}
+        English={myInfoLanguages.English}
       />
     </div>
   );
