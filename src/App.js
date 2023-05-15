5621388873b8a5a72c25e02286f83ee673e164f8
edwincamilo <edcamilo2016@gmail.com>
2023-05-14 23:27:38 -0500

    formulario

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import OfMainTitles from './components/bodyMyTitles';
 import OfMainWorkExperience from './components/bodyWorkExperience';
 import OfMainHabilities from './components/bodyHabilities';
 import OfMainLenguages from './components/bodyLenguages';
+import OfMainFormulario from './components/bodyFormulario';
 
 
 function App() {
@@ -24,56 +25,25 @@ function App() {
             <br />
             <img className="logos--aplicaciones" src={logo} alt=" " />
           </div>
-          <div className="col-2" />
+          <div className="col-2"/>
           <div className="col-5">
             <div className="row">
               <OfMainAboutMeDes/>
-            <br />
-            <OfMainTitles />
-            <br />
-            <OfMainWorkExperience />
-            <br />
-            <OfMainHabilities />
-            <br />
-            <OfMainLenguages />
-          </div>
-        </div>
-      </div>
-    </div>
-    <br></br>
-    <br></br>
-    <div class="login-wrap">
-      <div class="login-html">
-        <input id="tab-1" type="radio" name="tab" class="sign-in" checked /><label for="tab-1" class="tab">Sign In</label>
-        <input id="tab-2" type="radio" name="tab" class="for-pwd" /><label for="tab-2" class="tab">Forgot Password</label>
-        <div class="login-form">
-          <div class="sign-in-htm">
-            <div class="group">
-            <label for="user" class="label">Username or Email</label>
-            <input id="user" type="text" class="input"/>
-          </div>
-          <div class="group">
-            <label for="pass" class="label">Password</label>
-            <input id="pass" type="password" class="input" data-type="password" />
-          </div>
-          <div class="group">
-            <input type="submit" class="button" value="Sign In" />
-          </div>
-          <div class="hr"></div>
-        </div>
-        <div class="for-pwd-htm">
-          <div class="group">
-            <label for="user" class="label">Username or Email</label>
-            <input id="user" type="text" class="input" />
-          </div>
-          <div class="group">
-            <input type="submit" class="button" value="Reset Password" />
-          </div>
-          <div class="hr"></div>
+              <br/>
+              <OfMainTitles />
+              <br/>
+              <OfMainWorkExperience />
+              <br/>
+              <OfMainHabilities />
+              <br/>
+              <OfMainLenguages />
             </div>
           </div>
         </div>
       </div>
+      <br/>
+      <br/>
+      <OfMainFormulario/>
     </div>
   );
 }
